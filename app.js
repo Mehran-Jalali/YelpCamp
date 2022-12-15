0eb85047ff9e37a290f2a9d6672179131d1a55b0
mehran <104734004+Mehran-Jalali@users.noreply.github.com>
2022-12-16 01:07:48 +0330

    Added some ejs file

--- app.js
+++ app.js
@@ -37,13 +37,15 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "My Backyard",
-    description: "cheap camp",
-  });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/show", { campground });
 });
 
 app.listen(3000, () => {
