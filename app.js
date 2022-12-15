e45a479e713b2581857a04a1eeaa66d13930b5fe
mehran <104734004+Mehran-Jalali@users.noreply.github.com>
2022-12-15 22:34:20 +0330

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const Campground = require("./models/campground");
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Mongo connection open!!`);
+  })
+  .catch((err) => {
+    console.log("Mongo connection ERROR!!");
+    console.log(err);
+  });
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//   console.log("Database connected");
+// });
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//========================
+app.use(express.urlencoded({ extended: true }));
+//========================
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "cheap camp",
+  });
+  await camp.save();
+  res.send(camp);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
