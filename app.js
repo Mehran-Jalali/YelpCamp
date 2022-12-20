f3b7e0c66c4c18b3d97d589d5ef065a824cdc0d7
mehran <104734004+Mehran-Jalali@users.noreply.github.com>
2022-12-20 23:43:55 +0000

    Added primary bootstrap and Images from unsplash

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.delete("/campgrounds/:id", async (req, res) => {
   await Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds");
 });
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
--- seeds/index.js
+++ seeds/index.js
@@ -25,9 +25,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: "http://source.unsplash.com/collection/484351",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia corrupti necessitatibus sequi explicabo aperiam aspernatur tempore adipisci fuga est. Aliquam?",
+      price,
     });
     await camp.save();
   }
