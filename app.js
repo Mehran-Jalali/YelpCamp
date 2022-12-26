260b47425a38b468a22c5d96ca3af32ae9797dc8
mehran <104734004+Mehran-Jalali@users.noreply.github.com>
2022-12-26 16:58:46 +0330

    Added simple Error Template and basic Express error handling

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+//*************ERROR HANDLING****************
+const catchAsync = require("./utility/catchAsync");
+const ExpressError = require("./utility/ExpressError");
+//--------------------------------------------
 const path = require("path");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
@@ -42,47 +46,79 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
 
 // _1_ Add-New route
 app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
-app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
-});
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError("Invalid Campground Data", 400);
+    const campground = new Campground(req.body);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
 // _2_ Show route :
-app.get("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/show", { campground });
-});
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/show", { campground });
+  })
+);
 
 // _3_ Edit/Update route (so we need: 1=> "app.get()" to serve the form - 2=> "app.put")
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/edit", { campground });
-});
-app.put("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, { ...req.body });
-  res.redirect(`/campgrounds/${campground._id}`);
-});
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
 // _4_ Delete route
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+//-----------------------------Basic Error Handling
+app.all("*", (req, res, next) => {
+  next(new ExpressError("page not found", 404));
 });
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "OH NO, SOMETHING WENT WRONG!";
+
+  res.status(statusCode).render("error", { err });
+});
+//--------------------------------------------------
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
